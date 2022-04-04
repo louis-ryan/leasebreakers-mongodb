6bd8d292c11adf11655d592cc3d64de41c12f672
louisryan <louis.sw.ryan@gmail.com>
2022-04-04 20:08:57 +0200

    expanding api options

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,74 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 import { Button } from 'semantic-ui-react';
 
-const Index = ({ notes, users }) => {
+const Index = () => {
+
+  const [notes, setNotes] = useState([])
+
+
+  useEffect(() => {
+    async function getInitialNotes() {
+      const res = await fetch('https://leasebreakers-mongodb.hostman.site/api/notes');
+      const { data } = await res.json();
+
+      console.log("post data: ", data)
+      setNotes(data)
+    }
+    getInitialNotes()
+  }, [])
+
+
+  useEffect(() => {
+    async function getInitialUsers() {
+      const res = await fetch('https://leasebreakers-mongodb.hostman.site/api/users');
+      const { data } = await res.json();
+
+      console.log("user data: ", data)
+    }
+    getInitialUsers()
+  }, [])
+
+
   return (
     <div>
       <h1>Lease Breakers Melbourne</h1>
       <div>
         {notes.map(note => {
           return (
-            <div 
+            <div
               key={note._id}
-              style={{ width: "100%", border: "4px solid pink", padding: "24px", margin: "8px 0px"}}
+              style={{ width: "100%", border: "4px solid pink", padding: "24px", margin: "8px 0px" }}
             >
               <div>
-                  <div>
-                    <Link href={`/${note._id}`}>
-                      <a>{note.title}</a>
-                    </Link>
-                  </div>
-                </div>
                 <div>
                   <Link href={`/${note._id}`}>
-                    <Button primary>View</Button>
+                    <a>{note.address && note.address}</a>
                   </Link>
-                  <Link href={`/${note._id}/edit`}>
-                    <Button primary>Edit</Button>
-                  </Link>
+                  {console.log("note, ", note)}
+                  <div style={{ display: "flex" }}>
+                    {note.pics && note.pics.map(pic => {
+                      return (
+                        <div key={pic._id}>
+                          <img
+                            alt="note image"
+                            src={pic.url}
+                            style={{ width: "80px" }}
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <Link href={`/${note._id}`}>
+                  <Button primary>View</Button>
+                </Link>
+                <Link href={`/${note._id}/edit`}>
+                  <Button primary>Edit</Button>
+                </Link>
               </div>
             </div>
           )
@@ -34,22 +76,6 @@ const Index = ({ notes, users }) => {
       </div>
     </div>
   )
-}
-
-Index.getInitialProps = async () => {
-  const res = await fetch('https://leasebreakers-mongodb.hostman.site/api/notes');
-  const { data } = await res.json();
-
-  console.log("post data: ", data)
-  return { notes: data }
-}
-
-Index.getInitialProps = async () => {
-  const res = await fetch('https://leasebreakers-mongodb.hostman.site/api/users');
-  const { data } = await res.json();
-
-  console.log("user data: ", data)
-  return { users: data }
 }
 
 export default Index;
