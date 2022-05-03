2fd749d60c4c706a53d51ce04f3cfef7e9f0726a
louisryan <louis.sw.ryan@gmail.com>
2022-05-03 18:07:09 +0200

    added parking to model and UI update

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,10 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 import Layout from '../components/Layout';
 
 import 'semantic-ui-css/semantic.min.css'
+
 import '../css/style.css';
+import '../css/new.css';
+import '../css/Navbar.css';
 
 function MyApp({ Component, pageProps }) {
 
